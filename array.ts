8b673b80557fd0c36ac4e9dccdede89766f4c985
KarlangaXZ <carlos.josewin@gmail.com>
2024-09-15 18:54:07 -0400

    Agregando array

---
+++ array.ts
@@ -0,0 +1,13 @@
+let arreglo1: string[] = ["Pepe", "Arturo", "Sebastian", "Marcos"];
+let arreglo2: number[] = [2, 30, 215, 0.89, 12.35];
+let arreglo3: any[] = ["Pedro", "Juan", 32, 0.15, "Almohada", 24];
+
+console.log(arreglo1[3])
+
+arreglo2.push(2000)
+console.log(arreglo2)
+
+
+for (let i of arreglo3) {
+  console.log(i);
+}
