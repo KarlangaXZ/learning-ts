6516231c5ceed795507d424b1e51ad4e47033c92
KarlangaXZ <carlos.josewin@gmail.com>
2024-09-15 19:03:57 -0400

    condition add

---
+++ condition.js
@@ -0,0 +1,7 @@
+var algo = 'Alguna task planificada';
+if (algo.includes('tarea')) {
+    console.log('Esta palabra existe en esta cadena de texto');
+}
+else {
+    console.log('NO!, nada semejante en esta cadena de texto');
+}
---
+++ condition.ts
@@ -0,0 +1,8 @@
+let algo: string = 'Alguna task planificada'
+
+if(algo.includes('tarea')){
+    console.log('Esta palabra existe en esta cadena de texto')
+}
+else {
+    console.log('NO!, nada semejante en esta cadena de texto')
+}
