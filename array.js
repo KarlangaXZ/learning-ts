b38bb95bd82a8ac953da9ea451bcaeff4c13ac77
KarlangaXZ <carlos.josewin@gmail.com>
2024-09-17 15:40:48 -0400

    Encapsulamiento & Genericos

--- array.js
+++ array.js
@@ -1,10 +1,10 @@
-var arreglo1 = ["Pepe", "Arturo", "Sebastian", "Marcos"];
-var arreglo2 = [2, 30, 215, 0.89, 12.35];
-var arreglo3 = ["Pedro", "Juan", 32, 0.15, "Almohada", 24];
+let arreglo1 = ["Pepe", "Arturo", "Sebastian", "Marcos"];
+let arreglo2 = [2, 30, 215, 0.89, 12.35];
+let arreglo3 = ["Pedro", "Juan", 32, 0.15, "Almohada", 24];
 console.log(arreglo1[3]);
 arreglo2.push(2000);
 console.log(arreglo2);
-for (var _i = 0, arreglo3_1 = arreglo3; _i < arreglo3_1.length; _i++) {
-    var i = arreglo3_1[_i];
+for (let _i = 0, arreglo3_1 = arreglo3; _i < arreglo3_1.length; _i++) {
+    let i = arreglo3_1[_i];
     console.log(i);
 }
